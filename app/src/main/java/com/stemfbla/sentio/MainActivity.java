0eba72f133abfb34ee2994f07b4ab60bc9e958fa
apoorvk <apoorvknd@yahoo.com>
2015-03-03 00:26:31 -0800

    Added Navigation Drawer

---
+++ app/src/main/java/com/stemfbla/sentio/MainActivity.java
@@ -0,0 +1,75 @@
+package com.stemfbla.sentio;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment
+        .NavigationDrawerCallbacks, HomeFragment.OnFragmentInteractionListener,
+        CalendarFragment.OnFragmentInteractionListener, ClubsFragment.OnFragmentInteractionListener {
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private CharSequence mTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment;
+        switch(position) {
+            default:
+            case 0:
+                fragment = new HomeFragment();
+                break;
+            case 1:
+                fragment = new CalendarFragment();
+                break;
+            case 2:
+                fragment = new ClubsFragment();
+                break;
+        }
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch(number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+    @Override
+    public void onFragmentInteraction(android.net.Uri uri) { }
+}
--- app/src/main/java/com/stemfbla/sentio/SplashActivity.java
+++ app/src/main/java/com/stemfbla/sentio/SplashActivity.java
@@ -11,7 +11,7 @@ public class SplashActivity extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                startActivity(new Intent(SplashActivity.this, com.stemfbla.sentio.MainActivity.class));
                 finish();
             }
         }, 2000);
