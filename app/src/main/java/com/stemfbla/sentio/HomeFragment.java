01c9182115d41a75e1e2d32af517bd8da3869a3b
surajb300 <surajb300@gmail.com>
2015-03-08 13:19:44 -0700

    suraj_experiment

--- app/src/main/java/com/stemfbla/sentio/HomeFragment.java
+++ app/src/main/java/com/stemfbla/sentio/HomeFragment.java
@@ -1,11 +1,13 @@
 package com.stemfbla.sentio;
 import android.app.Activity;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
@@ -25,7 +27,16 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textview1=(TextView) view.findViewById(R.id.textview1);
+        textview1.setOnClickListener(new View.OnClickListener() {
+                     @Override
+                     public void onClick(View v) {
+                     textview1.setTextColor(Color.RED);
+                                         }
+                                     }
+        );
+        return view;
     }
 
     public void onButtonPressed(Uri uri) {
