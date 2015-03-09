abceb0f5d12176e3493df6c5efc0acbc9ca7a349
apoorvk <apoorvknd@yahoo.com>
2015-03-09 04:05:16 -0700

    Switch-case

--- app/src/main/java/com/stemfbla/sentio/ClubPageActivity.java
+++ app/src/main/java/com/stemfbla/sentio/ClubPageActivity.java
@@ -10,5 +10,37 @@ public class ClubPageActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club_page);
         int clubNumber = getIntent().getExtras().getInt("page");
+        switch(clubNumber) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+        }
     }
 }
