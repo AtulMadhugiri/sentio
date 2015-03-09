d153dc032a083b918b30dfa0c289ea49c6e20d62
apoorvk <apoorvknd@yahoo.com>
2015-03-08 23:27:38 -0700

    Added slideshow to home

--- app/src/main/java/com/stemfbla/sentio/HomeFragment.java
+++ app/src/main/java/com/stemfbla/sentio/HomeFragment.java
@@ -8,7 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
+
+    private com.daimajia.slider.library.SliderLayout sliderShow;
     private OnFragmentInteractionListener mListener;
+
     public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
         return fragment;
@@ -25,6 +28,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        sliderShow = (com.daimajia.slider.library.SliderLayout) view.findViewById(R.id.slider);
+        int file_maps[] = new int[10];
+        //file_maps.put(string, int);
+        sliderShow.setDuration(5000);
+        /*
+        for(){
+            com.daimajia.slider.library.SliderTypes.TextSliderView textSliderView = new com.daimajia.slider.library.SliderTypes.TextSliderView(this);
+            textSliderView
+                    .image(file_maps.get(name))
+                    .setScaleType(com.daimajia.slider.library.SliderTypes.BaseSliderView.ScaleType.Fit);
+            sliderShow.addSlider(textSliderView);
+        }
+        */
         return view;
     }
 
