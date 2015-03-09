d7f8ca68afa7b6368a2025b7961f7bdeff4eee06
apoorvk <apoorvknd@yahoo.com>
2015-03-09 00:03:36 -0700

    Finished home slideshow

--- app/src/main/java/com/stemfbla/sentio/HomeFragment.java
+++ app/src/main/java/com/stemfbla/sentio/HomeFragment.java
@@ -29,18 +29,22 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         sliderShow = (com.daimajia.slider.library.SliderLayout) view.findViewById(R.id.slider);
-        int file_maps[] = new int[10];
-        //file_maps.put(string, int);
+        int file_maps[] = new int[6];
+            file_maps[0] = R.drawable.stem3;
+            file_maps[1] = R.drawable.stem1;
+            file_maps[2] = R.drawable.stem0;
+            file_maps[3] = R.drawable.stem2;
+            file_maps[4] = R.drawable.stem4;
+            file_maps[5] = R.drawable.stem5;
         sliderShow.setDuration(5000);
-        /*
-        for(){
-            com.daimajia.slider.library.SliderTypes.TextSliderView textSliderView = new com.daimajia.slider.library.SliderTypes.TextSliderView(this);
+        for(int a = 0; a < file_maps.length; a++){
+            com.daimajia.slider.library.SliderTypes.TextSliderView textSliderView = new com
+                    .daimajia.slider.library.SliderTypes.TextSliderView(getActivity());
             textSliderView
-                    .image(file_maps.get(name))
+                    .image(file_maps[a])
                     .setScaleType(com.daimajia.slider.library.SliderTypes.BaseSliderView.ScaleType.Fit);
             sliderShow.addSlider(textSliderView);
         }
-        */
         return view;
     }
 
