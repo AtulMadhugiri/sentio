e95c27457092d2e4e14a1dc55472536cfcd91aa5
apoorvk <apoorvknd@yahoo.com>
2015-03-09 03:10:27 -0700

    New ClubPageActivity

---
+++ app/src/main/java/com/stemfbla/sentio/ClubPageActivity.java
@@ -0,0 +1,13 @@
+package com.stemfbla.sentio;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class ClubPageActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_page);
+    }
+}
